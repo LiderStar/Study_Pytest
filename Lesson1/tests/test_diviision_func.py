ce2bac277a4543797af4cad3b279d2f4ed59ad92
Volodymir Zybin <v_admin@ukr.net>
2023-04-20 20:29:04 +0300

    first test

--- Lesson1/tests/test_diviision_func.py
+++ Lesson1/tests/test_diviision_func.py
@@ -3,5 +3,20 @@ import pytest
 from Lesson1.divi import division
 
 
-def test_div():
-    assert division(10, 2) == 5.0
+@pytest.mark.parametrize("a,b,result", [(10, 2, 5),
+                                        (20, 2, 10),
+                                        (10, -2, -5)])
+def test_div(a, b, result):
+    assert division(a, b) == result
+
+
+@pytest.mark.parametrize("excepted, first, second", [(ZeroDivisionError, 10, 0),
+                                                     (TypeError, 10, "2")])
+def test_div_zr(excepted, first, second):
+    with pytest.raises(excepted):
+        division(first, second)
+
+
+def test_div_str():
+    with pytest.raises(TypeError):
+        division(10, "2")
